c18e25e0efdc944e55968d3cb7e327b3921f0ce3
sandyclark <joki0924@outlook.com>
2019-10-14 01:53:59 +0800

    테마작업 해결한 내용: light blue 내놓은 나머지 색상들 없애버림 color switcher 없애버림 navbar 에서 search bar 와 full screen 버튼 없애버림 언어선택 select 를 오른쪽으로 밀어붙임 모바일용 logo 그림 추가 default button style 을 flat 로 설정함

--- dev/frontend/haomibo/src/App.vue
+++ dev/frontend/haomibo/src/App.vue
@@ -1,19 +1,17 @@
 <template>
 <div class="h-100">
     <router-view />
-    <color-switcher />
 </div>
 </template>
 
 <script>
-import ColorSwitcher from './components/Common/ColorSwitcher'
 import {
     getDirection
 } from './utils'
 
 export default {
     components: {
-        'color-switcher': ColorSwitcher
+
     },
     beforeMount() {
         const direction = getDirection()
