48c9abe8fdd7c7a78d4ef896713c8e29c0339b1d
jgj0906 <56434546+jgj0906@users.noreply.github.com>
2019-11-10 23:16:35 +0800

    수정-태브 여백 -정광진 11/10

--- dev/frontend/haomibo/src/views/app/permission-management/organization-management.vue
+++ dev/frontend/haomibo/src/views/app/permission-management/organization-management.vue
@@ -38,9 +38,9 @@
     <b-tabs nav-class="ml-2" :no-fade="true" class="h-100">
 
       <b-tab :title="$t('permission-management.organization-table')">
-        <b-row v-if="pageStatus==='table'" class="h-100 mt-4">
+        <b-row v-if="pageStatus==='table'" class="h-100">
           <b-col cols="12 d-flex flex-column">
-            <b-row>
+            <b-row class="pt-4">
               <b-col class="d-flex">
                 <div class="flex-grow-1">
 
