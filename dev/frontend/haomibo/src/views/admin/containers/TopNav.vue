b732a077451b9943324465b9bb3a34068dc6d474
jgj0906 <56434546+jgj0906@users.noreply.github.com>
2019-12-17 20:42:02 +0800

    password reset page  permission point by jgj 12/17

--- dev/frontend/haomibo/src/views/admin/containers/TopNav.vue
+++ dev/frontend/haomibo/src/views/admin/containers/TopNav.vue
@@ -44,14 +44,49 @@
             </span>
             <span class="name ml-1 mr-2">{{currentUser.name}}</span>
           </template>
-          <b-dropdown-item>{{this.$t('menu.account')}}</b-dropdown-item>
+          <b-dropdown-item @click="showPasswordResetView()">{{this.$t('menu.account')}}</b-dropdown-item>
         </b-dropdown>
       </div>
       <div class="d-inline-block">
         <img src="/assets/img/turn_on_icon.svg" class="ml-5 mb-1 logout" @click="logout"/>
       </div>
     </div>
+    <b-modal centered id="modal-reset" ref="modal-reset">
+      <b-row>
+        <b-col cols="12" class="d-flex justify-content-center">
+          <div style="width: 300px">
+            <h3 class="text-center font-weight-bold">{{$t('password-reset.password-change')}}</h3>
+            <b-form-group class="mw-100">
+              <template slot="label">{{$t('password-reset.user-account')}}<span
+                class="text-danger"></span>
+              </template>
+              <b-form-input disabled v-model="passwordForm.userAccount" class="mw-100"></b-form-input>
+            </b-form-group>
+            <b-form-group class="mw-100">
+              <template slot="label">{{$t('password-reset.new-password')}}<span
+                class="text-danger">*</span>
+              </template>
+              <b-form-input type="password" v-model="passwordForm.password" :state="!$v.passwordForm.password.$dirty ? null : !$v.passwordForm.password.$invalid" class="mw-100"></b-form-input>
+            </b-form-group>
+            <b-form-group>
+              <template slot="label">{{$t('password-reset.confirm-password')}}<span
+                class="text-danger">*</span>
+              </template>
+              <b-form-input type="password" v-model="passwordForm.confirmPassword" :state="!$v.passwordForm.confirmPassword.$dirty ? null : !$v.passwordForm.confirmPassword.$invalid" class="mw-100"></b-form-input>
+            </b-form-group>
+          </div>
+        </b-col>
+      </b-row>
+      <template slot="modal-footer">
+        <b-button variant="primary default" @click="savePassword" class="mr-1">
+          {{$t('password-reset.confirm')}}
+        </b-button>
+        <b-button variant="light default" @click="hideModal('modal-reset')">{{$t('system-setting.cancel')}}
+        </b-button>
+      </template>
+    </b-modal>
   </nav>
+
 </template>
 
 <script>
@@ -64,12 +99,25 @@
   import {getDirection, removeLoginInfo, setDirection} from '../../../utils'
   import {getApiManager} from "../../../api";
   import {responseMessages} from "../../../constants/response-messages";
+  import {validationMixin} from 'vuelidate';
 
+  const {required, minLength, sameAs} = require('vuelidate/lib/validators');
   export default {
     components: {
       'menu-icon': MenuIcon,
       'mobile-menu-icon': MobileMenuIcon,
       'switches': Switches
+    },
+    mixins: [validationMixin],
+    validations: {
+      passwordForm: {
+        password: {
+          required, minLength: minLength(6)
+        },
+        confirmPassword: {
+          required, sameAs: sameAs('password')
+        },
+      }
     },
     data() {
       return {
@@ -77,16 +125,54 @@
         menuHiddenBreakpoint,
         localeOptions,
         notifications,
-        portrait: ''
+        portrait: '',
+        passwordForm: {
+          userAccount: null,
+          password: null,
+          confirmPassword: null
+        }
       }
     },
-    mounted () {
+    mounted() {
       this.portrait = `${apiBaseUrl}${this.currentUser.portrait}`;
+      this.passwordForm.userAccount = `${this.currentUser.name}`;
     },
     methods: {
       ...mapMutations(['changeSideMenuStatus', 'changeSideMenuForMobile']),
       ...mapActions(['setLang']),
 
+      showPasswordResetView() {
+        this.$refs['modal-reset'].show();
+      },
+      savePassword() {
+        this.$v.passwordForm.$touch();
+        if (this.$v.passwordForm.$invalid) {
+          return;
+        }
+        getApiManager()
+          .post(`${apiBaseUrl}/auth/change-password` , {
+            password:this.passwordForm.password
+          })
+          .then((response) => {
+            let message = response.data.message;
+            switch (message) {
+              case responseMessages['ok']: // okay
+                this.$notify('success', this.$t('permission-management.success'), this.$t(`password-reset.update-password-successful`), {
+                  duration: 3000,
+                  permanent: false
+                });
+                this.$refs['modal-reset'].hide();
+                this.passwordForm.password = null;
+                this.passwordForm.confirmPassword = null;
+                break;
+            }
+          })
+          .catch((error) => {
+          });
+      },
+      hideModal(modal) {
+        this.$refs[modal].hide();
+      },
       portraitOnError(e) {
         this.portrait = '/assets/img/user_placeholder.png';
       },
